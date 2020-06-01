38211c6e8b031df1342dd788e6cccd34dc158978
embydextrous <arjit.agarwal@meesho.com>
2020-06-01 21:31:38 +0530

    Data classes extensions

--- dataClasses.kt
+++ dataClasses.kt
@@ -1,5 +1,11 @@
 /* Provides hash code, equals, toString, getters and setters (for vars), copy(), componentN() functions */
-data class DataUser(val id: Int, val firstName: String, val lastname: String)
+/* primary constructors have vals and vars and in case of JVM they should provide default values */
+/* primary constructors must have one member */
+/* Data classes cannot be abstract, sealed, open or inner */
+/* Data classes can only implement interfaces */
+data class DataUser(val id: Int, val firstName: String, val lastname: String) {
+    var gender = "MALE" // this property does not take part in equals()/hashcode()/toString()/copy()
+}
 
 class SimpleUser(val id: Int, val firstName: String, val lastname: String)
 
@@ -17,12 +23,14 @@ fun main() {
 
     val c = DataUser(1, "Arjit", "Agarwal")
     val d = DataUser(1, "Arjit", "Agarwal")
+    d.gender = "FEMALE"
     run {
         // prints DataUser(id=1, firstName=Arjit, lastname=Agarwal)
         println(c)
         println(d)
         // prints true as we have equals() implementation
-        println(c == d)
+        println("Are genders same? ${c.gender == d.gender}") // false
+        println(c == d) // still true
         println("${c.component2()} ${c.component3()}")
     }
 }
---
+++ visibilityModifiers.kt
@@ -0,0 +1,36 @@
+open class A {
+    private val a = 1 // visible only inside class
+    val b = 2 // public accessible outside
+    protected var c = 3 //accessible inside subclasses
+
+    fun foo() {
+        val d = D()
+        // println("${d.e}") // e is private in D
+        println("f is ${d.f}")
+    }
+
+    inner class D {
+        private val e = 4
+        val f = 5
+        
+        init {
+            println("a: $a") // private members accessible to inner class 
+        }
+    }
+}
+
+class B : A() {
+    fun doSomething() {
+        // println("a is $a") // a is private in A
+        println("c is $c")
+    }
+}
+
+fun main() {
+    val a = A()
+    val b = B()
+    // println("a is ${a.a}") // a is private
+    println("b is ${a.b}")
+    a.foo()
+    // println("c is ${a.c}") // c is visible to subclasses only
+}
